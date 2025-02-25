db1399240047669c7258ddd61ea1a0161dfd6c6b
Sensationz Media and Arts Pvt. Ltd. <sensationzdigital@gmail.com>
2025-02-25 13:54:50 +0530

    ALL done

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,17 +1,5 @@
 // Hero component
 import {
-  client2,
-  client3,
-  client4,
-  client5,
-  client6,
-  client7,
-  client8,
-  client9,
-  client10,
-  client11,
-  client12,
-  client13,
   sketch,
   jewellery,
   rubik,
